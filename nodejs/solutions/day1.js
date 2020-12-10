fb4c0cc2322ddddc73e090b41c46b1540f7d36de
R Zardon Roger <zroger@novetta.com>
2020-12-10 13:33:44 -0800

    refactored day 1

--- nodejs/solutions/day1.js
+++ nodejs/solutions/day1.js
@@ -16,8 +16,8 @@ function byTwo() {
 }
 
 function byThree() {
-  for (let i = 0; i < data.length - 1; i++) {
-    for(ii = i + 1; ii < data.length; ii++) {
+  for (let i = 0; i < data.length - 2; i++) {
+    for(ii = i + 1; ii < data.length - 1; ii++) {
       for(iii = ii + 1; iii < data.length; iii++) {
         const x = data[i]
         const y = data[ii]
