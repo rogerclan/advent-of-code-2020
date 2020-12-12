e5f0753561f269acbf6554e4c88600ac0cc117d5
R Zardon Roger <zroger@novetta.com>
2020-12-11 22:51:28 -0800

    added comments to day 11

--- python/day12.py
+++ python/day12.py
@@ -9,21 +9,29 @@ y = 0
 # 0 = n, 1 = e, 2 = s, 3 = w
 d = 1
 
+# for row in data
 for r in data:
+    # command
     c = r[0]
+    # number
     n = r[1]
     if c == "F":
+        # east
         if d == 1:
             x += n
+        # west
         elif d == 3:
             x -= n
+        # north
         elif d == 0:
             y += n
+        # south
         else:
             y -= n
     elif c == "R":
         d = (d + 1 * (n / 90)) % 4
     elif c == "L":
+        # 3 * number of positions is one less than full rotation per position
         d = (d + 3 * (n / 90)) % 4
     elif c == "E":
         x += n
