0a49a045c33a383587e6766b7d2f62e210b33d6f
R Zardon Roger <zroger@novetta.com>
2020-12-14 06:59:47 -0800

    added link to how it works

--- python/day13.py
+++ python/day13.py
@@ -14,14 +14,16 @@ for bus in buses:
 print(best_bus[0] * best_bus[1])
 
 buses = ["x" if x == "x" else int(x) for x in data[1].split(",")]
-bus_to_offset = {bus: -i % bus for i, bus in enumerate(buses) if bus != "x"}
+bus_offsets = {bus: -i % bus for i, bus in enumerate(buses) if bus != "x"}
 # list of bus numbers large to small
-bus_numbers = list(reversed(sorted(bus_to_offset)))
+bus_numbers = list(reversed(sorted(bus_offsets)))
 # Offset of the bus time
-offset = bus_to_offset[bus_numbers[0]]
+offset = bus_offsets[bus_numbers[0]]
 bus = bus_numbers[0]
 for next_bus in bus_numbers[1:]:
-    while offset % next_bus != bus_to_offset[next_bus]:
+    # Chinese Remainder Theorem
+    # https://www.youtube.com/watch?v=ru7mWZJlRQg
+    while offset % next_bus != bus_offsets[next_bus]:
         offset += bus
     bus *= next_bus
 
